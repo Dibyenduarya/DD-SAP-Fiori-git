2330cc64b18664a9663c869256ec2c9b0c5390d3
Dibyendu Arya Diptam <arya.jsr@gmail.com>
2026-07-05 08:20:37 +0000

    change,live change, message toast, message box , function validation

--- salesorderapp.ui/webapp/controller/DisplayProduct.controller.js
+++ salesorderapp.ui/webapp/controller/DisplayProduct.controller.js
@@ -1,7 +1,9 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/m/MessageBox",
 ],
-function (Controller) {
+function (Controller, MessageToast, MessageBox) {
     "use strict";
 
     return Controller.extend("salesorderapp.ui.controller.DisplayProduct", {
@@ -17,7 +19,51 @@ function (Controller) {
             var oArgs = oEvent.getParameter("arguments");
             var sProductId = oArgs.ProductId;
             // Do something with the product ID
+        },
+
+        onUpdateProduct: function () {
+            debugger;
+            var oSelectedProductModel = this.getOwnerComponent().getModel("SelectedProductModel");
+            var oSelectedProductData = oSelectedProductModel.getData();
+
+        if (oSelectedProductData.Product_Text=="") {
+            MessageBox.warning("Product Name can not be blank. Cannot update product.");
+            MessageToast.show("Product Name can not be blank. Cannot update product.");
+            return; 
+        
         }
+        
+        
+        },
+
+        onMaxCapacityChange: function (oEvent) {
+            var sValue = oEvent.getParameter("value");
+            let iMaxLength = 7; // Set the maximum length to 7 digits
+            if (sValue.length > iMaxLength) {
+                MessageBox.warning("Maximum Capacity cannot exceed " + iMaxLength + " digits.");
+                MessageToast.show("Maximum Capacity cannot exceed " + iMaxLength + " digits.");
+                //oEvent.getSource().setValue(sValue.substring(0, iMaxLength)); // Trim the value to the maximum length
+            } else
+            if (sValue.length < 0) {
+                MessageBox.warning("Maximum Capacity cannot be negative.");
+                MessageToast.show("Maximum Capacity cannot be negative.");
+                //oEvent.getSource().setValue(0); // Reset to 0 or any other default value
+            }
+        },
+
+        onMaxCapacityLiveChange: function (oEvent) {
+            var sValue = oEvent.getParameter("value");
+            let iMaxLength = 7; // Set the maximum length to 7 digits
+            if (sValue.length > iMaxLength) {
+                MessageBox.warning("Maximum Capacity cannot exceed " + iMaxLength + " digits.");
+                MessageToast.show("Maximum Capacity cannot exceed " + iMaxLength + " digits.");
+                //oEvent.getSource().setValue(sValue.substring(0, iMaxLength)); // Trim the value to the maximum length
+            } else
+            {
+                MessageToast.show("Maximum Capacity is within the limit of" + iMaxLength + " digits.");
+                //oEvent.getSource().setValue(0); // Reset to 0 or any other default value
+            }
+        }   
 
     });
 });
